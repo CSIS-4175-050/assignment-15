05fb4d7ca1dc03d43fa82b6d76de642b2e4552c0
Gaurav Mehla <gaurav@mehla.in>
2023-07-20 01:21:04 -0700

    Todo App done

--- app/src/main/java/com/mehla/todolistdemo/MainActivity.java
+++ app/src/main/java/com/mehla/todolistdemo/MainActivity.java
@@ -1,14 +1,70 @@
 package com.mehla.todolistdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.mehla.todolistdemo.Adapter.ToDoAdapter;
+import com.mehla.todolistdemo.Model.ToDoModel;
+import com.mehla.todolistdemo.Utils.DatabaseHelper;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements OnDialogCloseListener{
+
+    private RecyclerView mRecyclerView;
+    private FloatingActionButton fab;
+    private DatabaseHelper myDB;
+
+    private List<ToDoModel> mList;
+
+    private ToDoAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = findViewById(R.id.recyclerView);
+        fab = findViewById(R.id.fab);
+        myDB = new DatabaseHelper(MainActivity.this);
+        mList = new ArrayList<>();
+        adapter = new ToDoAdapter(myDB, MainActivity.this);
+
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(adapter);
+
+        mList = myDB.getAllTasks();
+        Collections.reverse(mList);
+        adapter.setTask(mList);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddNewTask.newInstance().show(getSupportFragmentManager(), AddNewTask.TAG);
+            }
+        });
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new RecyclerViewTouchHelper(adapter));
+        itemTouchHelper.attachToRecyclerView(mRecyclerView);
+
+    }
+
+    @Override
+    public void onDilogClose(DialogInterface dialogInterface) {
+        mList = myDB.getAllTasks();
+        Collections.reverse(mList);
+        adapter.setTask(mList);
+        adapter.notifyDataSetChanged();
     }
 }
---
+++ app/src/main/java/com/mehla/todolistdemo/OnDialogCloseListener.java
@@ -0,0 +1,7 @@
+package com.mehla.todolistdemo;
+
+import android.content.DialogInterface;
+
+public interface OnDialogCloseListener {
+    void onDilogClose(DialogInterface dialogInterface);
+}
